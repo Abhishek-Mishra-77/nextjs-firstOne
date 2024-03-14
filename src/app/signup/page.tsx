d2d7c5141a79962ddf340f3f116259522f613cf3
Abhishek-Mishra-77 <abhishekmishra992016@gmail.com>
2024-03-14 17:15:29 +0530

    file setup done

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function SignUp() {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white text-2xl">SignUp</h1>
+    </div>
+  );
+}
