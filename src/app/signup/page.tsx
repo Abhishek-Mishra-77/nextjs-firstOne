f815205f89f916adba0358b17b5a08f8515a8885
Abhishek-Mishra-77 <abhishekmishra992016@gmail.com>
2024-03-14 17:25:53 +0530

    ignore

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
 export default function SignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
   return (
     <div className="flex">
       <h1 className="text-center text-white text-2xl">SignUp</h1>
