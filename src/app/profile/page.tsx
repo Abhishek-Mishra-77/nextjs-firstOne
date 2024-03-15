249b5870e3259be0249bbf91f578e39470b92ea2
Abhishek-Mishra-77 <abhishekmishra992016@gmail.com>
2024-03-15 17:50:17 +0530

    logout Done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,89 @@
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
-};
+"use client";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
-export default ProfilePage;
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function ProfilePage() {
+
+  const router = useRouter();
+
+  const onLogoutHandler = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push('/login')
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Profile Page
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {"Pin a footer to the bottom of the viewport."}
+          {"The footer will move as the main element of the page grows."}
+        </Typography>
+        <Typography variant="body1">Profile Page placeholder.</Typography>
+        <Button
+          onClick={onLogoutHandler}
+          sx={{ mt: 8, mb: 2 }}
+          variant="outlined"
+          color="error"
+        >
+          Logout
+        </Button>
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
